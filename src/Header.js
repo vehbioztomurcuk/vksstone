7e54ec9e451d84f9f3a3d22982dc221b5131b405
vehbioztomurcuk <vehbi.oztomurcuk@gmail.com>
2023-12-20 21:59:40 +0300

    process

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
-// src/Header.js
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './App.css'; // Import the CSS file
+import logoImage from './logo-raw.png'; // Import the logo image using require
 
 const Header = () => {
   const [showDropdownGallery, setShowDropdownGallery] = useState(false);
@@ -9,44 +10,70 @@ const Header = () => {
 
   return (
     <header>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li onMouseEnter={() => setShowDropdownGallery(true)} onMouseLeave={() => setShowDropdownGallery(false)}>
-            Gallery
-            {showDropdownGallery && (
-              <ul className="dropdown">
-                <li><Link to="/gallery/1">Product 1</Link></li>
-                <li><Link to="/gallery/2">Product 2</Link></li>
-                {/* ... more gallery items */}
-              </ul>
-            )}
-          </li>
-          <li onMouseEnter={() => setShowDropdownFactory(true)} onMouseLeave={() => setShowDropdownFactory(false)}>
-            Factory
-            {showDropdownFactory && (
-              <ul className="dropdown">
-                {/* Links to factory image galleries */}
-                <li><Link to="/factory/gallery">Factory Gallery</Link></li>
-              </ul>
-            )}
-          </li>
-          <li onMouseEnter={() => setShowDropdownProjects(true)} onMouseLeave={() => setShowDropdownProjects(false)}>
-            Projects
-            {showDropdownProjects && (
-              <ul className="dropdown">
-                {/* Links to project image galleries */}
-                <li><Link to="/projects/gallery">Projects Gallery</Link></li>
-              </ul>
-            )}
-          </li>
-          <li><Link to="/contact">Contact</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li>
-            <Link to="/companies">Companies</Link>
-          </li>
-        </ul>
-      </nav>
+      <div className="nav-container">
+        <Link to="/" className="logo">
+          <img src={logoImage} alt="Logo" />
+        </Link>
+        <nav>
+          <ul className="nav-menu">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li
+              onMouseEnter={() => setShowDropdownGallery(true)}
+              onMouseLeave={() => setShowDropdownGallery(false)}
+            >
+              Gallery
+              {showDropdownGallery && (
+                <ul className="dropdown">
+                  <li>
+                    <Link to="/gallery/1">Product 1</Link>
+                  </li>
+                  <li>
+                    <Link to="/gallery/2">Product 2</Link>
+                  </li>
+                  {/* Add more gallery items */}
+                </ul>
+              )}
+            </li>
+            <li
+              onMouseEnter={() => setShowDropdownFactory(true)}
+              onMouseLeave={() => setShowDropdownFactory(false)}
+            >
+              Factory
+              {showDropdownFactory && (
+                <ul className="dropdown">
+                  <li>
+                    <Link to="/factory/gallery">Factory Gallery</Link>
+                  </li>
+                </ul>
+              )}
+            </li>
+            <li
+              onMouseEnter={() => setShowDropdownProjects(true)}
+              onMouseLeave={() => setShowDropdownProjects(false)}
+            >
+              Projects
+              {showDropdownProjects && (
+                <ul className="dropdown">
+                  <li>
+                    <Link to="/projects/gallery">Projects Gallery</Link>
+                  </li>
+                </ul>
+              )}
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/companies">Companies</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
